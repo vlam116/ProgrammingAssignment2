6877ca1509b8936a10ba6f53c2e1019ef447fc31
vlam116 <vlam116@gmail.com>
2020-03-25 20:38:58 -0700

    created makeCacheMatrix and cacheSolve, added comments

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix.R allows the user to calculate the inverse matrix of a given matrix x, under the assumption
+## that x is invertible. The user may also set/get the values of x and its inverse matrix through the extract
+## operator $. Subsequently, the new inverse matrix generated in this way can be quickly recalculated and
+## retrieved. 
 
-## Write a short comment describing this function
+
+## Creates a list containing the "getter/setter" functions. The "set" and "get" functions set/get the 
+## matrix x, and the setIM and getIM do the same for the inverse of x. 
 
 makeCacheMatrix <- function(x = matrix()) {
-
+      IM = NULL
+      set = function(y) {
+           x <<- y
+           IM <<- NULL
+      }
+      get = function() x
+      setIM = function(solve) IM <<- solve
+      getIM = function() IM
+      list(set = set, get = get,
+           setIM = setIM,
+           getIM = getIM)
 }
 
 
-## Write a short comment describing this function
+## Calculates the inverse matrix of the list created by makeCacheMatrix, but first checks whether the inverse
+## matrix was already calculated. If IM already exists, prints a message and the IM. Otherwise, it gets the 
+## matrix x, calculates the inverse, sets it, then returns it. 
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+        IM = x$getIM()
+        if(!is.null(IM)) {
+              message("Retrieving from cache")
+              return(IM)
+        }
+        data = x$get()
+        IM = solve(data, ...)
+        x$setIM(IM)
+        IM
 }
